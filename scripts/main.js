e2c28a0649e5c96be948387c4a57e0d930420d76
unknown <kswedberg@nossi.edu>
2018-02-15 16:41:15 -0600

    2nd commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,22 @@
-$('#clickBtn').click(function(){
-	//$("#title").css("display","none");
-	$.getJSON("api.openweathermap.org/data/2.5/weather?zip=" + $("#inputZip").val() + , function(result){
-		for (var i = 0; i < result.length; i++){
-			$('#swatchPanel').append('<div class = "forecast">' + result.coord[i].lon + '</div>' );
+var url = "http://api.openweathermap.org/data/2.5/weather?appid=ffbaed99f10375462add667213061774&units=imperial&zip=";
 
-		}
+$('#checkBtn').click(function(){
+	if( $("#inputZip").val().length == 5){
+		$.getJSON(url + $("#inputZip").val(), function(result){	
+			$("#weatherIcon").css("display","block");
+			$("#cityName").html(result.name);
+			$("#conditions").html(result.weather[0].description);
+			$("#temp").html(result.main.temp + "°");
+			var d = new Date();
+			$("#time").html(d.toDateString() + "<br />" + d.toLocaleTimeString());
+			$("#weatherIcon").attr("src","http://openweathermap.org/img/w/" + result.weather[0].icon + ".png");
 	});
-	$("#clickBtn").val("Check Another Location?");
+
+	}else {
+			alert ("Please enter a valid zip code");
+			$("#inputZip").val() = "";
+	}
+			$("#checkBtn").val("Check Another Location?");
 });
 
 skrollr.init();
